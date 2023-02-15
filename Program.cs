ab4926728aa26e9cc0f3b983e2cbdf0b6c61dd59
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-02-15 21:28:58 +0530

    Welcome to Logical Programming

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using LogicalProgrammingg;
+
+namespace LogicalProgrammingg
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programming");
+
+            Fibonacci.DisplayFibonacciSeries();
+            PerfectNo.FindPerfectNumber();
+            PrimeNo.PrimeNumber();
+            ReverseNo.ReverseNumber();
+        }
+    }
+}
